205a29f8cdfdeee64330b9774e34b9ddec8e5b1c
JuanPaLiddell <juan.robles7638@alumnos.udg.mx>
2023-05-27 14:42:26 -0600

    correcion inyeccion sql

--- validateSQLInyection.js
+++ validateSQLInyection.js
@@ -1,10 +1,14 @@
+
+const isValidWord =(string)=> [";", "-", "'"].every((char) =>  (!string.includes(char)));
+
 /**
  * Validates if any of the strings received contains non valid characters that can be used in SQL injections
  * @param {Array of Strings} stringsArray 
  * @returns true if all the strings are valid, false otherwise.
  */
 export const validateSQLInyection = (stringsArray) => {
-    const isValidWord =(string)=> ([";", "-", "'"].forEach((char) =>  !string.includes(char)));
-    return stringsArray.every(isValidWord);
+    const response= stringsArray.every(isValidWord);
+    console.log('response: ',response);
+    return response;
 
 }
